0f2e429731c3c2b693df5a17b98af2485f87fd65
hleV <hlevqq@gmail.com>
2020-08-20 11:46:59 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,6 +3,6 @@ function insertInputValueAtSelection(input, value)
     input.focus();
 
     const start = input.selectionStart;
-    input.value = input.value.substring(0, start) + value + input.value.substring(input.selectionEnd);
+    input.value = `${input.value.substring(0, start)}${value}${input.value.substring(input.selectionEnd)}`;
     input.selectionStart = input.selectionEnd = start + value.length;
 }
