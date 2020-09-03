01738ab47395ffef9cafe752d882c89006ebf321
hleV <hlevqq@gmail.com>
2020-09-03 11:00:00 +0300

    Add validateEmail()

--- functions.js
+++ functions.js
@@ -5,3 +5,9 @@ function insertInputValueAtSelection(input, value)
     input.selectionStart = input.selectionEnd = oldSelectionStart + value.length;
     input.focus();
 }
+
+function validateEmail(email)
+{
+    const regex = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+    return email.match(regex);
+}
